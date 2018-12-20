507264b8fee4b178d1a511e24986f7dd264f52f2
mbarbuto <45580141+mbarbuto@users.noreply.github.com>
2018-12-19 22:05:50 -0500

    test js and css

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 window.onload = function() {
   const header = document.getElementById('header')
   header.innerText = 'Code goes here'
+  const p = document.createElement("p")
+  p.classList.add("feces")
+  p.innerText = "test"
+  document.body.appendChild(p)
 }
